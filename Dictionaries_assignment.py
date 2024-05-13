94cbb81f12c03d8917a4a554feb087d2a543da70
Chris-Nicki <christopher.nicki@gmail.com>
2024-05-13 12:02:11 -0600

    Works with some kinks

--- Dictionaries_assignment.py
+++ Dictionaries_assignment.py
@@ -17,31 +17,31 @@ dictionaries and manage data effectively.
 # Problem Statement:
 # # Given the initial menu:
 
-# restaurant_menu = {
-#     "Starters": {"Soup": 5.99, "Bruschetta": 6.50},
-#     "Main Course": {"Steak": 15.99, "Salmon": 13.99},
-#     "Desserts": {"Cake": 4.99, "Ice Cream": 3.99}
-# }
-# print(restaurant_menu)
-# print()
+restaurant_menu = {
+    "Starters": {"Soup": 5.99, "Bruschetta": 6.50},
+    "Main Course": {"Steak": 15.99, "Salmon": 13.99},
+    "Desserts": {"Cake": 4.99, "Ice Cream": 3.99}
+}
+print(restaurant_menu)
+print()
 
-# # Add a new category called "Beverages" with at least two items.
-# print("Beverages Added")
-# restaurant_menu["Beverages"] = {"Coke": 2.50, "Dr. Pepper": 2.50, "Sprite": 2.50}
-# print(restaurant_menu["Beverages"])
-# print(restaurant_menu)
-# print()
-# # Update the price of "Steak" to 17.99.
-# print("Steak price change")
-# restaurant_menu["Main Course"]["Steak"] = 17.99
-# new_price = restaurant_menu["Main Course"]["Steak"] = 17.99
-# item = "Steak"
-# print(f"Price of {item} changed to {new_price}")
-# print()
-# # Remove "Bruschetta" from "Starters"
-# print("Bruschetta Removed")
-# removed_item = restaurant_menu["Starters"].pop("Bruschetta") 
-# print(restaurant_menu)
+# Add a new category called "Beverages" with at least two items.
+print("Beverages Added")
+restaurant_menu["Beverages"] = {"Coke": 2.50, "Dr. Pepper": 2.50, "Sprite": 2.50}
+print(restaurant_menu["Beverages"])
+print(restaurant_menu)
+print()
+# Update the price of "Steak" to 17.99.
+print("Steak price change")
+restaurant_menu["Main Course"]["Steak"] = 17.99
+new_price = restaurant_menu["Main Course"]["Steak"] = 17.99
+item = "Steak"
+print(f"Price of {item} changed to {new_price}")
+print()
+# Remove "Bruschetta" from "Starters"
+print("Bruschetta Removed")
+removed_item = restaurant_menu["Starters"].pop("Bruschetta") 
+print(restaurant_menu)
 
 
 # 2. Python Programming Challenges for Customer Service Data Handling
@@ -104,14 +104,15 @@ def locate_a_ticket():
     while True:
         ticket_number = input("What is your ticket number? ") 
         if ticket_number in Ticket_Database:
-            print(Ticket_Database[ticket_number])# I need to define ticket so it knows what to print
+            print(Ticket_Database[ticket_number])
+            return ticket_number
         else:
             print("Please enter a valid ticket number")
         break
-# needs to locate by the key but bring up the whole key value pair
 def update_ticket():
-#    locate_a_ticket()
-    while True:
+    while True:   
+        ticket_number =locate_a_ticket() 
+        print(ticket_number)    
         update = input("""What would you like to do to the ticket?
 1. Change name
 2. Update issue
@@ -119,19 +120,40 @@ def update_ticket():
 4. Delete ticket
 """)
         if update == "1":
-            pass# I need this to bring change the first and last name
-                # then I need it to append to the name index in ticket dictionary
+            print("Please update your name")
+            update_first_name = input("What is your first name")
+            update_last_name = input("What is your last name? ")
+            Ticket_Database["open_tickets"][ticket_number]["Customer"] = [update_first_name, update_last_name]  
+                    # only works if the ticket is open, closed is not recognized
         elif update == "2":
-            pass# I need to call the current model and issue and change appropriate answer
-                # then I need it to append to the issues index in ticket dictionary
-        elif update == "3":
-            pass# I need to call the status ticket and update the status
-                # I then need to append the status index in the ticket dictionary
-        elif update == "4":
-            pass # Just remove the ticket from the dictionary all together
+            print("Please update the service needs. ")
+            n_model = input("What is the model number? ")
+            n_issue = input("What is the issue?")
+            Ticket_Database["open_tickets"][ticket_number]["Issue"]= [n_model, n_issue]
             break
-        else:
-            print("Please enter a valid entry.")
+        elif update == "3":
+            print("Please update the status of the ticket")
+            open = input("Would you like to mark the ticket open or closed? ")
+            while True:
+                if open == "open" or open == "closed":
+                    if open == "open":
+                        Ticket_Database["open_tickets"][ticket_number] = {"Status": [open]} 
+                    else:
+                        Ticket_Database["closed_tickets"][ticket_number] = {"Status": [open]}  
+                    break
+                else:
+                    open = input("Would you like to mark the ticket open or closed? ")  
+            break
+        # works but does not pop out of the old dictionary if moved to the new?
+        elif update == "4":
+            deleted_ticket = input("What ticket number would you like to delete? ")
+            if deleted_ticket in Ticket_Database:
+                deleted = Ticket_Database.pop,Ticket_Database["Open Tickets"].pop, Ticket_Database["closed_tickets"].pop(deleted_ticket)
+                print(f"{deleted} with {deleted_ticket} ticket has been deleted from database.")
+                break
+            else:
+                print("Please enter a valid entry.")
+        # works, but doesnt delete the ticket from every dictionary, can we do a mass wipe?
 
 def view_tickets():
     while True:
